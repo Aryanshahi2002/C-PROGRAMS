54477ff72589009d56da4d9be868203b5c9bc548
Aryanshahi2002 <103624440+Aryanshahi2002@users.noreply.github.com>
2022-04-23 21:37:33 +0530

    Add files via upload

---
+++ P_1_AREA_OF_RECTANGLE.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+int l, b, div;
+printf("Enter the value of length \n");
+scanf("%d", &l);
+printf("Enter the value of breadth \n");
+scanf("%d", &b);
+printf("AREA OF RECTANGLE IS: %d \n ", l*b);
+    return 0;
+}
